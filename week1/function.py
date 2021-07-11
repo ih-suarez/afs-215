0a736993e03d354fc6854fc7e436761d9165de87
ih-suarez <ismaelsrz@yahoo.com>
2021-07-10 19:56:24 -0500

    Function to find if a number is a perfect number

---
+++ week1/function.py
@@ -0,0 +1,24 @@
+def is_num_perfect():
+    # Number Input
+    num = int(input('Want to know if a number is a perfect number or not? Give me a number '))
+    # Making my List of Numbers from 1 to any given number 
+    # and calculating if they are divisible by the number given
+    numbers_divisible = [n for n in range(1, num) if num % n == 0]
+
+    # Adding the numbers from the list created
+    numbers_total = sum(numbers_divisible)
+
+    # If Else statement to determine whether the number given 
+    # is a perfect number
+    if numbers_total == num:
+        print(f'Dividers of {num}: {numbers_divisible}')
+        print(f'Sum of these numbers is = {numbers_total}')
+        print(f'{numbers_total} = {num}')
+        print(f'{num} is a Perfect Number')
+    else:
+        print(f'Dividers of {num}: {numbers_divisible}')
+        print(f'Sum of these numbers is = {numbers_total}')
+        print(f'{numbers_total} is not equal to {num}')
+        print(f'{num} is not a Perfect Number')
+
+is_num_perfect()
