edb2e7e59f9e2c81ae097f4ad76c9291999d9929
ih-suarez <ismaelsrz@yahoo.com>
2021-07-11 23:58:07 -0500

    FizzBuzz Project Set Up

---
+++ week1/FizzBuzz/FuzzBuzz.py
@@ -0,0 +1,11 @@
+num = int(input(f'Enter a number.. 1 - 2 '))
+
+def fizz_buzz(num):
+    if num == 1:
+        return f'Get {num}'
+    if num == 2:
+        return f'Get {num}'
+    return num
+
+print(fizz_buzz(num))
+
