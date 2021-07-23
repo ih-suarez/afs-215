2541171bfa03ec4562fa24f55560b655f63fb3fa
ih-suarez <ismaelsrz@yahoo.com>
2021-07-22 21:19:10 -0500

    comments added

--- week3/creating_a_server/createServer.js
+++ week3/creating_a_server/createServer.js
@@ -1,9 +1,12 @@
+// Importing and declaring http variable
 const http = require('http')
 
+// Declaring Server variable
 const server = http.createServer((req, res) => {
     console.log(`request made: ${req.url}`)
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('Hello World')
 });
 
+// Server is set to listen on port 9000
 server.listen(9000, '127.0.0.1', console.log('Now Listening on port 9000'))
